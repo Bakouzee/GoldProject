7475941e7662424cdc0d95c6fa19dd193191d13b
AngelSushi <angelsushigame@gmail.com>
2022-05-25 14:37:59 +0200

    PathFinding Work

--- Assets/Scripts/GridSystem/GridManager.cs
+++ Assets/Scripts/GridSystem/GridManager.cs
@@ -13,6 +13,9 @@ namespace GridSystem
         [SerializeField] private Tilemap tilemap;
         private Tile[,] tiles;
 
+        private bool testAI;
+        private Vector2 testStart, testAimed;
+
         private void Start()
         {
             Camera camera = Camera.main;
@@ -23,6 +26,12 @@ namespace GridSystem
             camera.orthographicSize = levelDimensions.x * 0.28f;
             
             GenerateGrid();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && testAI)
+                GetPath(testStart,testAimed);           
         }
 
         private void OnDrawGizmos()
@@ -55,9 +64,104 @@ namespace GridSystem
         {
             Stack<Direction> path = new Stack<Direction>();
 
+            Debug.Log("direct path");
+
+            testAI = true;
+            testStart = startPos;
+            testAimed = aimedPos;
+
+            // Il faut que a chaque fois on génére les 4 tiles dans chacune direction
+            // Puis calcule du H et du G
+
+            Tile origin = GetTileFromObjectPosition(startPos);
+            Tile end = GetTileFromObjectPosition(aimedPos);
+
+            Debug.Log("length: " + FindTilesNear(origin).Length);
             
-            
+            foreach(Tile tile in FindTilesNear(origin)) {
+                Debug.Log("tile: " + tile);
+                if(tile != null) {
+                    tile.GCost = GetDistance(origin, tile);
+                    tile.HCost = GetDistance(tile, end);
+
+                    Debug.Log("GCost: " + tile.GCost);
+                    Debug.Log("HCost: " + tile.HCost);
+                    Debug.Log("FCost: " + tile.FCost);
+                }
+            }
+
             return path;
+        }
+
+        private int GetDistance(Tile first,Tile second) {
+
+            Vector2Int firstCoord = FindTileCoords(first);
+            Vector2Int secondCoord = FindTileCoords(second);
+            
+            return Mathf.Abs((secondCoord.x - firstCoord.x) + (secondCoord.y - firstCoord.y));
+        }
+
+        private Tile[] FindTilesNear(Tile origin) {
+            Tile[] nearTiles = new Tile[4];
+
+            Vector2Int tileOriginCoords = FindTileCoords(origin);
+
+            nearTiles[0] = FindTileWithTileCoords(new int[] { tileOriginCoords.x,tileOriginCoords.y + 1});
+            nearTiles[1] = FindTileWithTileCoords(new int[] { tileOriginCoords.x - 1, tileOriginCoords.y });
+            nearTiles[2] = FindTileWithTileCoords(new int[] { tileOriginCoords.x + 1, tileOriginCoords.y });
+            nearTiles[3] = FindTileWithTileCoords(new int[] { tileOriginCoords.x, tileOriginCoords.y - 1 });
+
+            return nearTiles;
+
+        }
+
+        private Vector2Int FindTileCoords(Tile tile)  {
+            Vector2Int tileCoords = new Vector2Int();
+
+            for(int x = 0; x < tiles.GetLength(0);x++) {
+                for(int y = 0; y < tiles.GetLength(1);y++) {
+                    if(tiles[x,y] == tile) {
+                        tileCoords.x = x;
+                        tileCoords.y = y;
+                    }
+                
+                }
+            }
+
+            return tileCoords;  
+        }
+
+        private Tile FindTileWithTileCoords(int[] coords) {
+            foreach(Tile tile in tiles) {
+                if(FindTileCoords(tile)[0] == coords[0] && FindTileCoords(tile)[1] == 1)          
+                    return tile;   
+            }
+
+            return null;
+        }
+
+        // Permet de trouver la tile a la position donner 
+        private Tile GetTileFromObjectPosition(Vector3 objPosition) { // Position = coordonnées joueurs 
+            Tile tile = null;
+
+            foreach (Tile currentTile in tiles) {
+
+                if(currentTile != null && currentTile.TryGetComponent<SpriteRenderer>(out SpriteRenderer renderer)) {
+                    Vector3[] corners = { renderer.transform.TransformPoint(renderer.sprite.bounds.max), // topRight 
+                        renderer.transform.TransformPoint(new Vector3(renderer.sprite.bounds.max.x, renderer.sprite.bounds.min.y, 0)),// topLeft
+                        renderer.transform.TransformPoint(renderer.sprite.bounds.min), // bottomLeft
+                    };
+
+                    if ((int)objPosition.x >= (int)corners[1].x && (int)objPosition.x <= (int)corners[0].x)  {
+                        if ((int)objPosition.y >= corners[0].y && (int)objPosition.y <= corners[2].y)  {
+                            tile = currentTile;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return tile;
         }
     }
 }
--- Assets/Scripts/GridSystem/Tile.cs
+++ Assets/Scripts/GridSystem/Tile.cs
@@ -8,6 +8,14 @@ namespace GridSystem
     {
         [SerializeField] private GameObject highlight;
 
+        // Astar Variables
+        [HideInInspector]
+        public int GCost { get; set; } // Distance between begin tile to actual tile
+        [HideInInspector]
+        public int HCost; // Distance between actual tile and end tile (without obstacle)
+        [HideInInspector]
+        public int FCost { get => GCost + HCost; }
+
         private void Start()
         {
             if(highlight.activeSelf) highlight.SetActive(false);
@@ -16,6 +24,11 @@ namespace GridSystem
         private void OnMouseEnter()
         {
             highlight.SetActive(true);
+        }
+
+        private void OnMouseDown()
+        {
+            GridManager.Instance.GetPath(GameObject.FindGameObjectWithTag("Player").transform.position,transform.position);
         }
 
         private void OnMouseExit()
