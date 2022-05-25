5e816dc57802b1bbdfeb68ff1af56ec0bc8bed3b
Outfoxeed <letourneaumorigan@gmail.com>
2022-05-25 10:34:15 +0200

    FrighteningEvent base and IInteractable interface

---
+++ Assets/Scripts/FrighteningEvents/FrighteningEvent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GoldProject.FrighteningEvents
+{
+    public abstract class FrighteningEvent : MonoBehaviour, IInteractable
+    {
+        protected bool isTriggered = false;
+        protected bool inProgress = false;
+
+        #region Do
+        protected abstract IEnumerator DoActionCoroutine();
+        private IEnumerator DoCouroutine()
+        {
+            inProgress = true;
+            yield return DoActionCoroutine();
+            inProgress = false;
+            isTriggered = true;
+        }
+        public void Do()
+        {
+            if (inProgress || isTriggered)
+                return;
+            StartCoroutine(DoCouroutine());
+        }
+        #endregion
+
+        #region Undo
+        protected abstract IEnumerator UndoActionCoroutine();
+        private IEnumerator UndoCoroutine()
+        {
+            inProgress = true;
+            yield return UndoActionCoroutine();
+            inProgress = false;
+            isTriggered = false;
+        }
+        public void Undo()
+        {
+            if (inProgress || !isTriggered)
+                return;
+            StartCoroutine(UndoCoroutine());
+        }
+        #endregion
+
+        public bool IsInteractable => isTriggered && !inProgress;
+        public virtual void Interact()
+        {
+            if (!IsInteractable)
+                return;
+            
+            // To override
+        }
+    }
+}
