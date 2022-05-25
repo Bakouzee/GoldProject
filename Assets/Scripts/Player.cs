cc6b3082f2ba3de4811b01fc5d688777699f01f5
Outfoxeed <letourneaumorigan@gmail.com>
2022-05-25 14:46:35 +0200

    Entity class with GridControl and Localisation knowledge

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,30 +5,11 @@ using UnityEngine;
 
 namespace GoldProject
 {
-    public class Player : MonoBehaviour, ILocalizable
+    public class Player : Entity
     {
-        private Room currentRoom;
-        public Room CurrentRoom => currentRoom;
-
         private void Start()
         {
             currentRoom = RoomsManager.Instance.FirstRoom;
-        }
-        
-        private Collider2D lastCollider2D = null;
-        private void OnTriggerStay2D(Collider2D other)
-        {
-            if (lastCollider2D == other)
-                return;
-
-            Room newRoom = RoomsManager.Instance.GetColliderRoom(other);
-            if (newRoom != null)
-            {
-                currentRoom = newRoom;
-                Debug.Log(newRoom.name);
-            }
-
-            lastCollider2D = other;
         }
     }
 }
