e79e1ee1b1eded6e19940dc4b1cd4caf867fc456
Outfoxeed <letourneaumorigan@gmail.com>
2022-05-25 11:52:24 +0200

    Enemy Profile Scriptable Object

---
+++ Assets/Scripts/Editor/EnemyProfileEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace GoldProject.Editor
+{
+    [CustomEditor(typeof(EnemyProfile))]
+    public class EnemyProfileEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            GUILayout.Space(10);
+            
+            EnemyProfile enemyProfile = target as EnemyProfile;
+
+            Texture2D texture = AssetPreview.GetAssetPreview(enemyProfile.sprite);
+            if (texture == null)
+                return;
+            GUILayout.Label("", GUILayout.Width(200), GUILayout.Height(200));
+            GUI.DrawTexture(GUILayoutUtility.GetLastRect(), texture);
+
+        }
+    }
+}
