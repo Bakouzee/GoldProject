3bc155c3f1f5e7d6b71f21f117c9669d95ea8e77
Freddy <freddy_maupou@orange.fr>
2022-06-01 11:50:32 +0200

    WIP knighttrap

--- Assets/Scripts/FrighteningEvents/KnightEvent.cs
+++ Assets/Scripts/FrighteningEvents/KnightEvent.cs
@@ -7,6 +7,7 @@ using GridSystem;
 public class KnightEvent : FrighteningEventBase
 {
     public List<Direction> directionKnight = new List<Direction>();
+
     private void OnMouseDown()
     {
         Do();
@@ -17,13 +18,18 @@ public class KnightEvent : FrighteningEventBase
     {
         //get the closest enemy pos so the knight will move to the enemy --> HAVE TO MAKE THE KNIGHT MOVE AT
         // THE SAME TIME OF THE ENEMIES
-        Vector2Int enemyToScare = CurrentRoom.GetClosestEnemy(transform.position).GridPosition;
+        Vector2Int enemyToScare = CurrentRoom.GetClosestEnemy(transform.position).GridController.gridPosition;
         Vector2Int knightPos = GridManager.Instance.GetGridPosition(transform.parent.position);
+
+        //Get the path to do
         directionKnight = GridManager.Instance.TempGetPath(knightPos, enemyToScare);
 
-         ;
+        foreach(Direction direction in directionKnight)
+        {
+            //Move the gameobject itself but not the parent
+            gridController.Move(direction);
+        }
 
-        Debug.Log("Activated");
         yield return new WaitForSeconds(50f);
         Debug.Log("done");
     }
