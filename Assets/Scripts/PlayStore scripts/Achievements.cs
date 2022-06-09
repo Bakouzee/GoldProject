5e3d52f5aa634db363311a2fee6166b0d94c2508
Outfoxeed <letourneaumorigan@gmail.com>
2022-06-09 15:25:53 +0200

    Imported GooglePlay Services and first tests

---
+++ Assets/Scripts/PlayStore scripts/Achievements.cs
@@ -0,0 +1,19 @@
+﻿using GooglePlayGames;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+namespace GoldProject
+{
+    public static class Achievements
+    {
+        public static readonly string BOO = "CgkI06WkmfgFEAIQAQ";
+
+        public static void Unlock(string achievementId)
+        {
+            Social.ReportProgress(achievementId, 100.0d, success =>
+            {
+                // Handle success or not
+            });
+        }
+    }
+}
