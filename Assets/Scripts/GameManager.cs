6765c7a179a0705bf3e3359d9e50ba6c3a282b88
Freddy <freddy_maupou@orange.fr>
2022-06-09 11:28:59 +0200

    Bye bye debug :'(

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -168,7 +168,6 @@ public class GameManager : SingletonBase<GameManager>
                 enemy.DoAction();
             }
 
-            Debug.Log("Count of knights : " + EnemyManager.knights.Count);
             // Knight too
             foreach (var knight in EnemyManager.knights)
             {
